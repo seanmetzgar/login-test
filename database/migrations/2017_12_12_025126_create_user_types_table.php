72645cf7a618249df9a12d4022573e8bd1f23218
Sean Metzgar <sean.metzgar@gmail.com>
2017-12-12 22:52:07 -0500

    Initial Commit

---
+++ database/migrations/2017_12_12_025126_create_user_types_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->string('description');
+            $table->timestamps();
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedInteger('user_type_id')->default(1);
+            $table->foreign('user_type_id')->references('id')->on('user_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_types');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('user_type_id');
+        });
+    }
+}
